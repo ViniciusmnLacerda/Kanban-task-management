b171d8d00bb8d6723af68bdd8822cbf0e1901eb2
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-13 22:27:02 -0300

    fix: login page styles

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -29,7 +29,7 @@ export default function Login() {
         <div className="logo">
           <IoPersonOutline />
         </div>
-        <h1>Login</h1>
+        <h1>Kanban</h1>
         <form>
           <label className="lbl" htmlFor="email">
             <input
