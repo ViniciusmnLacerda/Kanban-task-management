1496d5581900144daa45b31762cd979a0601c24d
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-15 10:25:15 -0300

    feat: new users on user seeders

--- backend/src/database/seeders/20230213140652-users.js
+++ backend/src/database/seeders/20230213140652-users.js
@@ -1,3 +1,4 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('users', [
@@ -7,10 +8,20 @@ module.exports = {
           // password: MyPassword
       },
       {
-        email: 'maria@email.com',
+        email: 'zita@email.com',
         password: '$2a$10$dun3xUEzvxq.Gz5kJUrw/el7os8EuLVsu/odPALS1y.CNI7HcXAui', 
           // password: MyPassword
-        },
+      },
+      {
+        email: 'marianne@email.com',
+        password: '$2a$10$dun3xUEzvxq.Gz5kJUrw/el7os8EuLVsu/odPALS1y.CNI7HcXAui', 
+          // password: MyPassword
+      },
+      {
+        email: 'igor@email.com',
+        password: '$2a$10$dun3xUEzvxq.Gz5kJUrw/el7os8EuLVsu/odPALS1y.CNI7HcXAui', 
+          // password: MyPassword
+      },      
     ], {});
   },
 
