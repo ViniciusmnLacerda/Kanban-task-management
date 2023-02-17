d8e1fdaaf088bc0b4de4a1eb6d04957c50cebda8
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-17 15:49:44 -0300

    refact: members controller and workspaces controller implement IController generics interface

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,13 +1,9 @@
 import AccountController from './Account.controller';
 import LoginController from './Login.controller';
-import MembersController from './Members.controller';
 import RegisterController from './Register.controller';
-import WorkspacesController from './Workspaces.controller';
 
 export {
   LoginController,
   RegisterController,
   AccountController,
-  WorkspacesController,
-  MembersController,
 };
