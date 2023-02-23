3036f5b4736e8a5f765380766c08ae8ef04ea87a
Vinícius Lacerda <viniciuslmn@gmail.com>
2023-02-23 10:45:38 -0300

    feat: route to workspace page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Workspace from './pages/Workspace';
 
 export default function App() {
   return (
@@ -18,6 +19,10 @@ export default function App() {
         <Route
           path="/home"
           element={ <Home /> }
+        />
+        <Route
+          path="/workspace/:workspaceId"
+          element={ <Workspace /> }
         />
       </Routes>
     </BrowserRouter>
